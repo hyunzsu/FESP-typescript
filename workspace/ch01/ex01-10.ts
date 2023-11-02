dc3c01ecf5fe8b3c3294f9c4e44d43103d55f27a
hyunzsu <kkm5169@naver.com>
2023-11-02 09:33:37 +0900

    TypeScript day 1 오후

--- workspace/ch01/ex01-10.ts
+++ workspace/ch01/ex01-10.ts
@@ -1,5 +1,25 @@
 // 인터페이스
 // ex01-10.ts 복사
-(()=>{
-    
+(() => {
+  // 객체 타입 선언
+  interface User {
+    name: string;
+    age: number;
+  }
+
+  // 변수에 타입 지정
+  var kim: User = { name: '김철수', age: 30 };
+
+  // 함수 매개변수에 타입 지정
+  var getAge = function (kim: User) {
+    return kim.age;
+  };
+
+  // 함수의 리턴 타입 지정
+  var createUser = function (name: string, age: number): User {
+    return { name, age };
+  };
+
+  var kim = createUser('김철수', 30);
+  console.log(getAge(kim));
 })();
